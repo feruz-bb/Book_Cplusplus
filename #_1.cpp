acc1fdde16428c1ee826e925423be524011a94f8
Feruzbek Baqoyev <baqoyevferuzbek1005@gmail.com>
2024-03-02 16:36:46 +0500

    C++

---
+++ #_1.cpp
@@ -0,0 +1,113 @@
+// Muallif: Baqoyev Feruzbek Barakayevich
+// Sana: 2 - mart 2024-yil
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/*
+// Maqsad: a va b sonlarning yig`indisini topuvchi dastur
+int main(){
+	int a,b;
+	cin >> a;
+	cin >> b;
+	cout << "a + b = " << a+b;
+	return 0;
+}
+
+*/
+
+/*
+// Maqsad: Kubning hajmi va yon tomonini yuzasini hisoblash
+int main(){
+	int a;
+	cout << "a = ";cin >> a;
+	cout <<endl<< "V = " << pow(a,3);
+	cout <<endl<< "S = "<< 6*pow(a,2);
+	return 0;
+}
+*/
+
+/*
+// Maqsad: uchburchakning gipotenuzasi va yuzini hisoblash
+int main(){
+	int a,b;
+	float c,S;
+	cout << "a = ";cin >> a;
+	cout << endl;
+	cout << "b = "; cin >> b;
+	cout << "c = " << sqrt(pow(a,2) + pow(b,2)) << endl;
+	cout << "S = " << a*b/2;
+	return 0; 
+}
+*/
+
+/*
+// Maqsad: 2 ta sonning o`rta arifmetigi va o`rta giometrigi
+int main(){
+	int a,b;
+	float arif;
+	float geo;
+	cin >>a;
+	cin >> b;
+	cout << "arifmetik = " << (a+b)/2;
+	cout << endl << "geo = " << sqrt(a*b); 
+	return 0;
+}
+*/
+
+/*
+// Maqsad: uchish vaqtini topish
+int main(){
+	float H,t;
+	int g = 10;
+	cout << "H = ";cin >> H;
+	cout << "t = " << sqrt(2*H/g);
+	return 0;
+}
+*/
+
+/*
+// Maqsad: teng tomonli uchburchakning yuzasini topish
+int main(){
+	float a;
+	cout << "a = ";cin >> a;
+	cout << endl ;
+	cout << "S = " << sqrt(3)*pow(a,2)/4;
+	return 0;
+}
+*/
+
+/*
+// Maqsad: L uzunlikdagi matematik mayatnikning tebranish davri
+int main(){
+	float L, pi = 3.14;
+	int g = 10;
+	cout << "L = ";cin >> L;
+	cout << endl << "T = " << 2*pi*sqrt(L/g);
+	return 0;
+}
+*/
+
+/*
+// Maqsad: 3 ta parallel ulangan qarshiliklarni umumiy qarshiligi
+int main(){
+	float R1, R2, R3, Ru;
+	cout << "R1 = "; cin >> R1;
+	cout << endl << "R2 = "; cin >> R2;
+	cout << endl << "R3 = "; cin >> R3;
+	cout << "Ru = " << (R1*R2*R3)/(R1*R2 + R2*R3 + R1*R3);
+	return 0;
+}
+*/
+
+/*
+//Maqsad: ifoda programmasini tuzish
+int main(){
+	float x, y, z;
+	cout << "x = ";cin >> x;
+	cout << endl << "y = "; cin >> y;
+	cout << "z = " << (fabs(x)-fabs(y))/(1+fabs(x*y));
+	return 0;
+}
+*/
