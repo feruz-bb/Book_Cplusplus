55e9807af43769b21e2721fc14e865f9a729363b
Feruzbek Baqoyev <baqoyevferuzbek1005@gmail.com>
2024-03-15 11:05:48 +0500

    new tasks

---
+++ #3-Butun-va-haqiqiy-sonlar-ustida-amallar.cpp
@@ -0,0 +1,60 @@
+// Sana: 15 - mart 2024 - yil
+// Muallif: Baqoyev Feruzbek Barakayevich
+// Maqsad: Butun va haqiqiy sonlarga doir masalalar ishlash
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+// TASK 1
+// Maqsad: santimetrdan metrga o`tish
+/*
+int main(){
+	float L;
+	cout << "L necha santimetr >>> "; cin >> L;
+	cout << "L = " << L / 100 << " metr"; 
+}
+
+*/
+
+// TASK 2
+// Maqsad: kg dan tonnaga o`tish
+/*
+int main(){
+	float kg;
+	cout << "m = "; cin >> kg;
+	cout << "M = " << kg / 1000 << " tonna";
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
